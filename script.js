f6959d7a4773b0f170374bee6d91fe435836e757
Priyanshi-Mittal-20 <priyanshimzn05@gmail.com>
2026-06-06 12:49:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your code here
+let priceArr=document.querySelectorAll(".price")
+let sum=0
+for(let i of priceArr)
+	{
+		sum=sum+Number(i.innerText)
+	}
+let table=document.querySelector("table")
+let row=document.createElement("tr")
+let data=document.createElement("td")
+data.innerText=sum
+data.setAttribute("data-ns-test","grandTotal")
+data.setAttribute("colspan","2")
+row.appendChild(data)
+table.appendChild(row)
